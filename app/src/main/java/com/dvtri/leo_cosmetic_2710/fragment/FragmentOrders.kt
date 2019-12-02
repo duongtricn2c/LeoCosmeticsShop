b0597f6d007a23ce2417215045396aa571e66287
duongtricn2c <duongtricn2c@gmail.com>
2019-12-03 00:16:31 +0700

    commit fragment item load list Orders

--- app/src/main/java/com/dvtri/leo_cosmetic_2710/fragment/FragmentOrders.kt
+++ app/src/main/java/com/dvtri/leo_cosmetic_2710/fragment/FragmentOrders.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dvtri.leo_cosmetic_2710.R
+import com.dvtri.leo_cosmetic_2710.data.Order
+import kotlinx.android.synthetic.main.fragment_orders.*
 
 class FragmentOrders : Fragment() {
-
+    var listOrders : ArrayList<Order> = ArrayList()
+    var adapter : OrderAdapter? = null
 
     companion object {
         /**
@@ -34,6 +38,74 @@ class FragmentOrders : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_orders, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initData()
+        initUI()
+    }
+
+    private fun initUI() {
+        adapter = OrderAdapter(listOrders)
+        rlvOrders.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, true)
+        rlvOrders.setHasFixedSize(true)
+        rlvOrders.adapter = adapter
+    }
+
+    private fun initData() {
+        listOrders.run {
+            add(
+                Order("ODS00001",
+                "Bùi Thúy Hoàng Oanh","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","1","Chưa thanh toán")
+            )
+            add(Order("ODS00002",
+                "Hoàng Anh Việt","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","1","Chưa thanh toán"))
+            add(Order("ODS00003",
+                "Nguyễn Thị Hoa","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","2","Chưa thanh toán"))
+            add(Order("ODS00004",
+                "Trần Anh Nhân","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","1","Chưa thanh toán"))
+            add(Order("ODS00005",
+                "Mai Thị Lựu Đạn","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","3","Chưa thanh toán"))
+            add(Order("ODS00006",
+                "Huỳnh Thị Phương","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","2","Chưa thanh toán"))
+            add(Order("ODS00007",
+                "Hồ Mỹ Trân","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","2","Chưa thanh toán"))
+            add(Order("ODS00008",
+                "Bùi Anh Tuấn","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","2","Chưa thanh toán"))
+            add(Order("ODS00009",
+                "Nguyễn Khởi My","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","3","Chưa thanh toán"))
+            add(Order("ODS00010",
+                "Lê Trúc Phương","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","1","Chưa thanh toán"))
+            add(Order("ODS00011",
+                "Mai Thị Nhàn","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","2","Chưa thanh toán"))
+            add(Order("ODS00012",
+                "Lê Dương Bảo Lâm","01/12/2019",
+                "02/12/2019",30000,"02/12/2019",
+                "59 Lê Thành Phương, TP Nha Trang","1","Chưa thanh toán"))
+        }
     }
 
 }
--- app/src/main/java/com/dvtri/leo_cosmetic_2710/fragment/OrderAdapter.kt
+++ app/src/main/java/com/dvtri/leo_cosmetic_2710/fragment/OrderAdapter.kt
@@ -1,8 +1,13 @@
 package com.dvtri.leo_cosmetic_2710.fragment
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.marginBottom
 import androidx.recyclerview.widget.RecyclerView
+import com.dvtri.leo_cosmetic_2710.R
 import com.dvtri.leo_cosmetic_2710.data.Order
 import com.dvtri.leo_cosmetic_2710.fragment.OrderAdapter.MyViewHolder as MyViewHolder
 
@@ -14,7 +19,7 @@ class OrderAdapter(listOrders: ArrayList<Order>) : RecyclerView.Adapter<MyViewHo
         this.listOrders = listOrders
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view_oders, parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -22,12 +27,37 @@ class OrderAdapter(listOrders: ArrayList<Order>) : RecyclerView.Adapter<MyViewHo
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val item = listOrders[position]
+        holder.tvOrderName.text = item.orderID +"_"+item.customerName
+        holder.tvAdress.text = item.shipAddress
+        holder.tvNote.text = item.note
+        when(item.status.toInt()){
+            1 -> {
+                holder.tvStatus.text = "Hoàn thành"
+                holder.imgStatus.setImageResource(R.drawable.ic_sucess)
+            }
+            2 -> {
+                holder.tvStatus.text = "Đang đợi"
+                holder.imgStatus.setImageResource(R.drawable.ic_waiting)
+            }
+            3 -> {
+                holder.tvStatus.text = "Đã hủy"
+                holder.imgStatus.setImageResource(R.drawable.ic_cancel)
+            }
+        }
+        if (position == 0){
+            holder.tvLine.visibility = View.VISIBLE
+        }
     }
 
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvOrderName = view.findViewById(R.id.tvOrderName) as TextView
+        val tvAdress = view.findViewById(R.id.tvAdress) as TextView
+        val tvNote = view.findViewById(R.id.tvNote) as TextView
+        val tvStatus = view.findViewById(R.id.tvStatus) as TextView
+        val tvLine = view.findViewById(R.id.tvLine) as TextView
+        val imgStatus = view.findViewById(R.id.imgStatus) as ImageView
     }
 
 }
